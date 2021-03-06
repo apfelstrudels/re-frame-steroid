7edc356ba0b5e1184fe7ded711c4bd19430d79f6
Claus Spacil <claus@spacil.com>
2021-03-06 18:07:16 +0100

    Update project.clj
    updated version numbers

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://github.com/flexsurfer/re-frame-steroid"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/clojurescript "1.10.520"]])
+  :dependencies [[org.clojure/clojure "1.10.2"]
+                 [org.clojure/clojurescript "1.10.741"]])
